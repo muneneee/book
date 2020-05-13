a48f307286102fe5e34ce6356818e87409d3473b
rmautia <raphaelnyangenya@gmail.com>
2020-05-13 15:48:03 +0300

    auth features

--- config.py
+++ config.py
@@ -1,5 +1,17 @@
+import os
+
 class Config:
-    pass
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static'
+
+
+    # email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
 
 class ProdConfig(Config):
     pass
