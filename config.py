7dc45ba53646dfe96f1f317ee08d0e7bd6fb09b3
Andrew59-boop <millerfaabi@gmail.com>
2020-05-15 00:23:19 +0300

    home page

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
+import os
+
 class Config:
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://foxx:birdd@localhost/home_landing'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = '1234'
+    UPLOADED_PHOTOS_DEST ='app/static'
+
+
+    # email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
 
 class ProdConfig(Config):
     pass
@@ -14,3 +28,4 @@ class DevConfig(Config):
 Config_options = {
     'development':DevConfig,
     'production':ProdConfig
+}
